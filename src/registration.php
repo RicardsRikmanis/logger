9d50d169d20686570b717b2e3cd7424673e2f4da
Ilja Lapkovskis <ilja.lapkovskis@gmail.com>
2020-03-19 15:53:54 +0200

    Add Magento registration

---
+++ src/registration.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * ScandiPWA_Logger
+ *
+ * @category    ScandiPWA
+ * @package     ScandiPWA_Logger
+ * @author      Ilja Lapkovskis <ilja@scandiweb.com | info@scandiweb.com>
+ * @copyright   Copyright (c) 2020 Scandiweb, Ltd (https://scandiweb.com)
+ * @license     OSL-3.0
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'ScandiPWA_Logger',
+    __DIR__
+);
